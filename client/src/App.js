2ae542915b22c140d853e1b168fbe4c1f7da0efe
AnnaMintel <annikamintel99@yandex.ru>
2022-12-07 20:00:52 +0300

    feat: added longpulling real time method

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Longpulling } from './Longpulling';
 
 function App() {
   return (
--- client/src/Longpulling.jsx
+++ client/src/Longpulling.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./Longpulling.css";
 
 export const Longpulling = () => {
-  <div className="center">
-    <div>
-      <div className="form"></div>
-      <input type="text" />
-      <button>Send</button>
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    subscribe();
+  }, []);
+
+  const subscribe = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5001/get-messages");
+      setMessages((prew) => [data, ...prew]);
+      await subscribe();
+    } catch (e) {
+      setTimeout(() => {
+        subscribe();
+      }, 500);
+    }
+  };
+
+  const sendMessage = async () => {
+    await axios.post("http://localhost:5001/new-messages", {
+      message: value,
+      id: Date.now(),
+    });
+  };
+
+  return (
+    <div className="center">
+      <div>
+        <div className="form">
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+          />
+          <button onClick={sendMessage}>Send</button>
+        </div>
+        <div>
+          {messages.map((mess) => (
+            <div className="message" key={mess.id}>
+              {mess.message}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
